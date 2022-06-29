d87e0036549f685d626e6a680eab004fcccf48be
kurodayuuki <kurodayuki07122170@gmail.com>
2022-06-29 18:27:45 +0900

    ダイアログ機能追加、レスポンシブ対応

--- src/components/task/button/TaskCardDeleteButton.jsx
+++ src/components/task/button/TaskCardDeleteButton.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const TaskCardDeleteButton = ({ taskCardsList, setTaskCardsList, taskCard }) => {
 
   const taskCardDeleteButton = (id) => {
-    //タスクカードを削除する
-    setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
-
+    const isBool = window.confirm("本当に削除しますか？");
+    if(isBool) {
+      //タスクカードを削除する
+      setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
+    }
   }
+
   return (
     <div>
       <button className='taskCardDeleteButton' onClick={() => taskCardDeleteButton(taskCard.id)}><i className='fas fa-times fa-lg'></i></button>
